e508369eb973afc74ef57d3c25b6c56801abc439
Yashraj0888 <bloodymoonknite888@gmail.com>
2023-10-13 10:37:44 +0530

    change

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ btnn.addEventListener('click',()=>{
     event.preventDefault();
     let container = document.getElementById("container");
     let paragraph = document.createElement("h1");
-    paragraph.innerHTML="45 lakh"
+    paragraph.innerHTML="35-45 lakh"
     container.appendChild(paragraph);
     paragraph.style.color="rgba(255, 179, 203, 1)"
     paragraph.style.paddingTop="10px";
